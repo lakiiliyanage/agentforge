19fefd172f8f8ec1677f0c868ecd2390ade7274b
Lakii Liyanage <liyanage.lakii@gmail.com>
2026-04-23 11:57:54 +0200

    docs: practice commit from week 3

--- src/components/practice/MessageCounter.tsx
+++ src/components/practice/MessageCounter.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState } from "react" /*test comment# test comment 2*/
 
 export default function MessageCounter() {
   const [count, setCount] = useState(10)
